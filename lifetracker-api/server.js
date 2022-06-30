f72c27ccfadf793ccaf24edf02caec0c9b3b81b5
Daniel Jonas <02danieljonas@gmail.com>
2022-06-30 13:36:11 -0400

    created some needed files and folders

---
+++ lifetracker-api/server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+// const {PORT} = require("./config")
+const authRoutes = require("./routes/auth")
