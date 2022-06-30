4cd1364825bb2433be6e0d3647fc38e966a22cff
Daniel Jonas <02danieljonas@gmail.com>
2022-06-30 17:43:17 -0400

    completed user auth

--- lifetracker-api/models/user.js
+++ lifetracker-api/models/user.js
@@ -1,21 +1,66 @@
-const { UnauthorizedError, NotFoundError, BadRequestError } = require("../utils/errors");
-const {BCRYPT_WORK_FACTOR} =require("../config")
+const {
+    UnauthorizedError,
+    NotFoundError,
+    BadRequestError,
+} = require("../utils/errors");
+const { BCRYPT_WORK_FACTOR } = require("../config");
 const bcrypt = require("bcrypt");
 
 const db = require("../db");
 
 class User {
+    static async makePublicUser(user) {
+        return {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            created_at: user.created_at,
+            update_at: user.update_at,
+        };
+    }
+
     static async login(credentials) {
         //user should submit email and passowrd
         //if any fileds are missing throw an error
 
+        if (!credentials) {
+            throw new BadRequestError(`Request body is wrong`);
+        }
+        //user should submit their info
+        //if any of this fields are missing throw an error
+        const requiredFiled = ["email", "password"];
+        requiredFiled.forEach((field) => {
+            if (!credentials.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body`);
+            }
+        });
+
         //then look up user in the db by email
+        const user = await User.fetchUserByEmail(credentials.email);
+
         //if a user is found compare the submitted password
         //with the password in the db
         //if there is a match return the user
+        if (user) {
+            console.log(credentials.password);
+            console.log(user.password);
+            const isValid = await bcrypt.compare(
+                credentials.password,
+                user.password
+            );
+            if (isValid) {
+                return User.makePublicUser(user);
+            }
+        } else {
+            const isValid = await bcrypt.compare(
+                credentials.password,
+                "$2b$13$aEFwdMYtTBwfmToeSBFz4OtrRcAGTUC3CjSsERRP4qO3A2Pk7Ivbq"
+            );
+        }
 
         //if any of this goes wrong throw an error
-
         throw new UnauthorizedError("Invalid email or password");
     }
     static async register(credentials) {
@@ -29,7 +74,7 @@ class User {
             "password",
             "firstName",
             "lastName",
-            "username"
+            "username",
         ];
         requiredFiled.forEach((field) => {
             if (!credentials.hasOwnProperty(field)) {
@@ -49,13 +94,13 @@ class User {
         }
 
         //take users password and hash it
-        const hashedPw = await bcrypt.hash(credentials.password, BCRYPT_WORK_FACTOR)
+        const hashedPw = await bcrypt.hash(
+            credentials.password,
+            BCRYPT_WORK_FACTOR
+        );
 
         //take users email and lowercase it
         const lowercaseEmail = credentials.email.toLowerCase();
-
-
-
 
         //create a new user in the db with all their info
         const result = await db.query(
@@ -75,11 +120,11 @@ class User {
                 hashedPw,
                 credentials.firstName,
                 credentials.lastName,
-                credentials.username
+                credentials.username,
             ]
         );
         const user = result.rows[0];
-        return user;
+        return User.makePublicUser(user);
     }
     static async fetchUserByEmail(email) {
         if (!email) {
--- lifetracker-api/routes/auth.js
+++ lifetracker-api/routes/auth.js
@@ -7,7 +7,7 @@ const { UnauthorizedError, BadRequestError } = require("../utils/errors");
 router.post("/login", async (req, res, next)=>{
     try{
         
-        const user = await User.loging(req.body)
+        const user = await User.login(req.body)
         return res.status(200).json({user})
     }catch(err){
         next(err)
